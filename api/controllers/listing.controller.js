09c6db9ad9388230c3b4981fbddfe14cdde652c0
areebqazi <66074608+areebqazi@users.noreply.github.com>
2024-02-22 01:57:07 +0530

    fixed some issues

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -2,9 +2,13 @@ import Listing from '../models/listing.model.js';
 
 export const createListing = async (req, res, next) => {
     try {
+      console.log('try')
+      console.log(req.body)
         const listing = await Listing.create(req.body);
         res.status(201).json(listing);
     } catch (error) {
+      console.log('catch')
+      console.log(error)
         next(error)
     }
 };
